96be7ae93a645b88e8e6e7a091a19ad305bc58ff
castillocarlosr <castillocarlosr@gmail.com>
2019-01-23 19:05:21 -0800

    done with this project

---
+++ MyFirstMVC/MyFirstMVC/Models/TimePerson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyFirstMVC.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+        
+        public static List<TimePerson> GetPersons(int startYear, int endYear)
+        {
+            List<TimePerson> people = new List<TimePerson>();
+            string path = Environment.CurrentDirectory;
+            string newPath = Path.GetFullPath(Path.Combine(path, @"C:\Users\casti\coding\codeFellows\code401\Lab11-MyFirstMVCApp\MyFirstMVC\MyFirstMVC\wwwroot\personOfTheYear.csv"));
+
+            string[] newFile = File.ReadAllLines(newPath);
+             
+            for (int i = 1; i < newFile.Length; i++)
+            {
+                string[] field = newFile[i].Split(','); ;
+
+                people.Add(new TimePerson
+                {
+                    //turinery opers
+                    Year = (field[0] == "") ? 0 : Convert.ToInt32(field[0]),
+                    Honor = field[1],
+                    Name = field[2],
+                    Country = field[3],
+                    BirthYear = (field[4] == "") ? 0 : Convert.ToInt32(field[4]),
+                    DeathYear = (field[5] == "") ? 0 : Convert.ToInt32(field[5]),
+                    Title = field[6],
+                    Category = field[7],
+                    Context = field[8],
+                });
+            }
+            List<TimePerson> displayOfPeople = people.Where(x =>(x.Year >= startYear) && (x.Year <= endYear)).ToList();
+            return displayOfPeople;
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Lab11-MyFirstMVCApp
 started Jan.22, 2019
+
+## Purpose:
+This is my first attempt at creating a working a basic MVC app using ASP.NET Core.
+
+##Summary
+This is to display a Time Magazine Person of the year and return results upon user input of years.
+
+### How to run the program:
+```
+This program requires visual studio 2017.
+Run the program to start the console application.
+This will be under "Program.cs".
+```
+
+You may look at the source code to ensure nothing evil is in there.
+```
+* The web browser will welcome you to a Front Page of Time Magazine.
+* The user then types in two numbers.  A starting year and end year.
+* Upon clicking Submit, the webpage will redirect the user to a results page.
+* The Results Page will display in a grid like pattern the people who were Time Magazine of those years.
+* When finished viewing, simply exit the web brower.
+```
+
+This assignment took about 6 hours to complete.  There are no xUnit Testing for this assignment.
+
+Below are screen shots of what this should look like.
+###Screen Shot 
+![](assets/Front.PNG?raw=true)
+![](assets/Results.PNG?raw=true)
+
+Lab started on January 23, 2019 by Carlos R. Castillo
+Thanks for stopping by and stay classy Seattle.
